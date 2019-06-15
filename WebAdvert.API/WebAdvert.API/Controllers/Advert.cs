eb45bb6b4ef25a161a0ae5bc12a3fee6cac6e781
JvitorNery <joao.victor.lemos.nery@gmail.com>
2019-06-15 17:12:12 -0300

    feat(master): creating api using DynamoDB and lib with shared models

---
+++ WebAdvert.API/WebAdvert.API/Controllers/Advert.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using WebAdvert.API.Services;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAdvert.API.Controllers
+{
+    [ApiController]
+    [Route("adverts/v1/")]
+    public class Advert : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public Advert(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(201, Type=typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId = "";
+            try
+            {
+                recordId = await _advertStorageService.Add(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+                throw;
+            }
+            return StatusCode(201, new CreateAdvertResponse { Id = recordId });
+        }
+
+        [HttpPut]
+        [Route("confirm")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+                throw;
+            }
+            return StatusCode(200);
+        }
+    }
+}
---
+++ WebAdvert.API/WebAdvert.API/Services/AdvertProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace WebAdvert.API.Services
+{
+    public class AdvertProfile : Profile
+    {
+        public AdvertProfile(string profileName)
+        {
+            CreateMap<AdvertDBModel, AdvertDBModel>();
+        }
+    }
+}
